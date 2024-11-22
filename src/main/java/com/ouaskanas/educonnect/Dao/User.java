c69e8aa98df1b033ded707a3fdf06b413afa1688
ouaskanas <anasouaskar2003@gmail.com>
2024-11-22 23:26:24 +0100

    initialisation

---
+++ src/main/java/com/ouaskanas/educonnect/Dao/User.java
@@ -0,0 +1,4 @@
+package com.ouaskanas.educonnect.Dao;
+
+public class User {
+}
---
+++ src/test/java/com/ouaskanas/educonnect/EduConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouaskanas.educonnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
