71555b3413f3d1234cad14c6a278b528d0065388
ouaskanas <anasouaskar2003@gmail.com>
2024-11-26 12:13:03 +0100

    repository fix

--- src/main/java/com/ouaskanas/educonnect/Dao/Repositories/CommentRepository.java
+++ src/main/java/com/ouaskanas/educonnect/Dao/Repositories/CommentRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    public List<Comment> findByAuthor(long author_id);
-    public List<Comment> findbyPost(long post_id);
 }
