7e4a154475afab276c075e6c5ec6c4057d3de913
ouaskanas <anasouaskar2003@gmail.com>
2024-12-25 23:28:11 +0100

    jwt filter

--- src/main/java/com/ouaskanas/educonnect/Security/SecurityConfig.java
+++ src/main/java/com/ouaskanas/educonnect/Security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -27,6 +28,7 @@ public class SecurityConfig {
                         .anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
